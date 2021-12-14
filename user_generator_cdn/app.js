3d093f7e33e39f423d6128cbc79ef702f9746b44
rokpopov <rokpopov90@gmail.com>
2021-12-14 21:55:30 +0100

    add a method in vue component that is tied to the onclick event handler and changes the user when the button is clicked

--- user_generator_cdn/app.js
+++ user_generator_cdn/app.js
@@ -7,6 +7,15 @@ const app = Vue.createApp({
       gender: 'female',
       picture: 'https://randomuser.me/api/portraits/men/37.jpg'
     }
+  },
+  methods: {
+    getUser() {
+      this.firstName = 'Henriette',
+      this.lastName = 'Meijs',
+      this.email = 'henriette@email.com',
+      this.gender = 'female',
+      this.picture = 'https://randomuser.me/api/portraits/men/59.jpg'
+    }
   }
 })
 
