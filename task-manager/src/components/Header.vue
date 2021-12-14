9070407acd486d7c69f8132c76bff64f1e6f57cd
rokpopov <rokpopov90@gmail.com>
2021-12-14 22:37:59 +0100

    initialize task_manager project, cleanup and create a new Header component, pass props to it and style it

---
+++ task-manager/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+</template>
+
+
+<script>
+  export default {
+    name: 'Header',
+    // props can be defined as an array or an object
+    // props: ['title']
+    props: {
+      title: String
+    }
+  }
+</script>
+
+// scoped means it is only be applicable to this specific component
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
