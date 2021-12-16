2da41ef3fbabf2db29226fbee4f4cd7d36f0686b
rokpopov <rokpopov90@gmail.com>
2021-12-16 21:29:25 +0100

    add props to button component, add event handler

--- task-manager/src/components/Button.vue
+++ task-manager/src/components/Button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button>Add Task</button>
+  <button @click="onClick()" class="btn" :style="{ background: color }">{{ text }}</button>
 </template>
 
 <script>
   export default {
-    name: 'Button'
+    name: 'Button',
+    props: {
+      text: String,
+      color: String
+    },
+    methods: {
+      onClick() {
+        console.log("You click! Oke see you.");
+      }
+    }
   }
 </script>
